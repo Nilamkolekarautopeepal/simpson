c89fe9040235f67181b8fd37ae225e2d6f55ba11
sattyaenata <sattyajit.kane@autopeepal.com>
2026-07-04 16:14:05 +0530

    flash,dtc,pid complete

--- lib/services/androidOperationservice.dart
+++ lib/services/androidOperationservice.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
